4c4b8c4bac00aee252aee30c630b91534b850106
Jason Garber <jason@sixtwothree.org>
2012-04-27 18:52:30 -0400

    Rewrote significant portions of the plugin to use two data attributes, allow for passing in optional selectors, added window.GoogleAnalytics (consider renaming this?), and added chainability.

--- jquery-googleanalytics.js
+++ jquery-googleanalytics.js
@@ -1,5 +1,5 @@
 /**
- * @name jQuery Google Analytics (https://github.com/jgarber623/jquery-googleanalytics)
+ * @name jQuery Google Analytics Event Tracking (https://github.com/jgarber623/jquery-googleanalytics)
  * @author Jason Garber
  * @copyright (cc) Jason Garber (http://sixtwothree.org and http://www.viget.com)
  * 
@@ -17,31 +17,43 @@
 	
 	GoogleAnalytics.prototype = {
 		defaults: {
+			selectors: "a, button",
 			separator: ", "
 		},
 		
 		init: function() {
 			this.config = $.extend( {}, this.defaults, this.options, this.metadata );
 			
-			this.$elem.on( "mousedown.googleanalytics keydown.googleanalytics", "a[data-track-event], button[data-track-event]", $.proxy( this.track, this ) );
+			var selectors_components = this.config.selectors.split( "," );
+			
+			for ( i = 0, j = selectors_components.length; i < j; i++ ) {
+				selectors_components[i] = $.trim( selectors_components[i] ) + "[data-track-method]";
+			}
+			
+			this.$elem.on( "mousedown.googleanalytics keydown.googleanalytics", selectors_components.join(), $.proxy( this.track, this ) );
+			
+			return this;
 		},
 		
-		push: function( tracking_components ) {
+		push: function( method, param_components ) {
 			if ( _gaq !== "undefined" ) {
-				_gaq.push( ["_trackEvent"].concat( tracking_components ) );
+				_gaq.push( [method].concat( param_components ) );
 			}
 		},
 		
 		track: function( event ) {
 			if ( event.type === "mousedown" || event.which === 13 ) {
 				var $target = $( event.currentTarget ),
-					tracking = $target.data( "track-event" ),
-					tracking_components = tracking.split( this.config.separator );
+					method = $target.data( "track-method" ),
+					params = $target.data( "track-params" ),
+					param_components = params.split( this.config.separator );
 				
-				tracking_components[3] = ( !tracking_components[3] || tracking_components[3] === "" ) ? 0 : parseFloat( tracking_components[3] );
-				tracking_components[4] = ( tracking_components[4] && tracking_components[4].toLowerCase() === "true" ) ? true : false;
+				if ( method === "_trackEvent" ) {
+					param_components[3] = ( !param_components[3] || param_components[3] === "" ) ? 0 : parseInt( param_components[3], 10 );
+					param_components[4] = ( param_components[4] && param_components[4].toLowerCase() === "true" ) ? true : false;
+				}
 				
-				this.push( tracking_components );
+				this.push( method, param_components );
 			}
 		}
 	};
@@ -53,5 +65,7 @@
 			new GoogleAnalytics( this, options ).init();
 		});
 	};
+	
+	window.GoogleAnalytics = GoogleAnalytics;
 	
 })( jQuery, window , document );
